6376fa4df7db5a5916099185300dc10d8990383f
Gauthier Denglos <gauthier.denglos@epita.fr>
2019-05-03 17:18:24 +0200

    Lon/Lat/Crhono add

--- trailXplorer/app/src/main/java/com/javahelps/trailxplorer/MainActivity.java
+++ trailXplorer/app/src/main/java/com/javahelps/trailxplorer/MainActivity.java
@@ -1,13 +1,32 @@
 package com.javahelps.trailxplorer;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    //Stopwatch variable. Allows to create and increment the timer
+    private Chronometer chronometer;
+    private long pauseOffset = 0;
+    private boolean running;
+
+    //Location: Altitude, longitude and Latitude
+    private TextView textview;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +36,87 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Start Activity");
 
+        chronometer = findViewById(R.id.chronometer);
+
         Button stopBtn = (Button) findViewById(R.id.launchActivity);
+        Button startBtn = (Button) findViewById(R.id.createGpx);
+        Button resetBtn = (Button) findViewById(R.id.resetAll);
         stopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, statActivity.class));
             }
         });
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startChronometer(v);
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetChronometer(v);
+            }
+        });
+
+        textview = (TextView) findViewById(R.id.id_textview);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //  ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            // public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+
+        onLocationChanged(location);
+    }
+
+    public void startChronometer (View v){
+        if(!running){
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+
+    }
+
+    public void resetChronometer (View v){
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        textview.setText("Longitude: " + longitude + "\n" + "Latitude: " + latitude);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 }
--- trailXplorer/app/src/main/java/com/javahelps/trailxplorer/statActivity.java
+++ trailXplorer/app/src/main/java/com/javahelps/trailxplorer/statActivity.java
@@ -1,8 +1,14 @@
 package com.javahelps.trailxplorer;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class statActivity extends AppCompatActivity {
 
